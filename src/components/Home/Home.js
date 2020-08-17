acfcef433637ae6a153f37ac53381f6ac3c6dfcb
Shamseena Karumarot <shamseenakarumarot@gmail.com>
2020-08-17 14:51:50 +0200

    Integrated axios GET request for GNEWSAPI

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React,{useEffect , useState} from 'react';
 import "./home.scss";
+import axios from "axios";
 
 
 function Home() {
+  const [data , setData] = useState([]);
+  useEffect(()=>{
+  	
+  	axios.get(`https://gnews.io/api/v3/top-news?&token=${process.env.REACT_APP_GNEWS_API_KEY}`)
+    .then(res =>{
+            
+           if(res.data){
+                    console.log("res",res);
+                }   
+           else{
+              console.log("ERROR");
+             
+                
+            } 
+
+
+
+
+  })
+},[]);
+
   return (
     <div className="container">
      
         <h4>Home</h4>
+
+
         
     </div>
   );
