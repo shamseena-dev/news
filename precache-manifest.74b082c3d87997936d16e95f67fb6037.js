90e23a2019bb6cbacf6cc5550935571444ce9711
Shamseena Karumarot <shamseenakarumarot@gmail.com>
2020-08-18 22:44:31 +0200

    Updates

---
+++ precache-manifest.74b082c3d87997936d16e95f67fb6037.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a3e310c5756cfb6126f83eba741a20",
+    "url": "/news/index.html"
+  },
+  {
+    "revision": "736b2a53208a34fcd8f0",
+    "url": "/news/static/css/main.451c58cb.chunk.css"
+  },
+  {
+    "revision": "e7773ef9c556d65e5b64",
+    "url": "/news/static/js/2.e55bd1f2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/news/static/js/2.e55bd1f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "736b2a53208a34fcd8f0",
+    "url": "/news/static/js/main.669ace93.chunk.js"
+  },
+  {
+    "revision": "7576b4ad0cd43891079c",
+    "url": "/news/static/js/runtime-main.7ab2d7f2.js"
+  },
+  {
+    "revision": "3f954894feced62891da3d75ad028e10",
+    "url": "/news/static/media/urban-min.3f954894.jpg"
+  }
+]);
