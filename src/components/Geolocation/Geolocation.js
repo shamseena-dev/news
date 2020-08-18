0a639255cb13a3f1df2d1cf316407e0bd684b165
Shamseena Karumarot <shamseenakarumarot@gmail.com>
2020-08-18 10:27:12 +0200

    Added navigator.language to get default Browser language

--- src/components/Geolocation/Geolocation.js
+++ src/components/Geolocation/Geolocation.js
@@ -1,5 +1,6 @@
 import React,{useEffect}from 'react';
 import "./geolocation.scss";
+import axios from "axios";
 
 
 function Geolocation() {
@@ -7,16 +8,31 @@ function Geolocation() {
 		navigator.geolocation.getCurrentPosition(function(position) {
       console.log("Lat", position.coords.latitude);
       console.log("Long", position.coords.longitude);
+       console.log("lang",navigator.language);
+
+       {/*
+          axios.get(`http://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&sensor=false`).
+          then(res=>{
+
+                    console.log(res);
+                  })
+
+               */}
+
     });
 	},[]);
   return (
-    <div className="container">
+    <div className="">
      
-        <h4> Geolocation</h4>
+        
         
     </div>
   );
 }
 
 export default Geolocation;
+
+
+
+
 
